39053ee9d4944b0ced9bcd32be0c59452124924c
Ella Cohen <93930110+ellacohen@users.noreply.github.com>
2021-12-01 01:37:42 +0000

    i set num= user's input, then did if num % 3 ==0 to print it is divisible, but if it is false, print not divisible. Then, i wrote if statements to further evaluate the number to see if it was positive negative or zero.

--- part3.py
+++ part3.py
@@ -20,3 +20,16 @@ not divisible by 3
 '''
 
 #write your code below
+num = int(input("Enter a number: "))
+
+if num % 3 == 0:
+  print ("divisible by 3")
+else: 
+  print ("not divisble by 3")
+
+if num >0:
+  print ("positive")
+if num ==0:
+  print ("zero")
+if num <0:
+  print ("negative")
